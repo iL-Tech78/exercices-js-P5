3b9d0848a872c09cb504f27366da47bb5a6c4502
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2024-07-01 16:45:39 +0200

    Exercices projet 5

---
+++ exo9/exo9.js
@@ -0,0 +1,13 @@
+function checkAge(age) {
+    if (age < 18) {
+        return "Vous êtes mineur.";
+    } else if (age >= 18 && age <= 65) {
+        return "Vous êtes majeur.";
+    } else {
+        return "Vous êtes senior.";
+    }
+}
+
+console.log(checkAge(15));   // "Vous êtes mineur."
+console.log(checkAge(30));   // "Vous êtes majeur."
+console.log(checkAge(75));   // "Vous êtes senior."
